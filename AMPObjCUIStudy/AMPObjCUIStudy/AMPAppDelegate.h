bfe3e24c7083c2a84ceac681271e31ad6bca469c
pilotns <pilot.ns@icloud.com>
2017-06-02 16:15:39 +0300

    AMPObjCUIStudy - project created

---
+++ AMPObjCUIStudy/AMPObjCUIStudy/AMPAppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AMPAppDelegate.h
+//  AMPObjCUIStudy
+//
+//  Created by pilotns on 02.06.17.
+//  Copyright © 2017 pilotns. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AMPAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ AMPObjCUIStudy/AMPObjCUIStudy/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  AMPObjCUIStudy
+//
+//  Created by pilotns on 02.06.17.
+//  Copyright © 2017 pilotns. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AMPAppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AMPAppDelegate class]));
+    }
+}
