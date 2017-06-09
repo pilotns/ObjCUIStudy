14e2dd5f01a35c013edff90b63303702ad871368
pilotns <pilot.ns@icloud.com>
2017-06-09 18:42:48 +0300

    AMPCGShortCuts, AMPMarcos, AMPRandom

--- AMPObjCUIStudy/Library/AMPMarcos.h
+++ AMPObjCUIStudy/Library/AMPMarcos.h
@@ -9,13 +9,37 @@
 #ifndef AMPMarcos_h
 #define AMPMarcos_h
 
+#import "AMPCGShortCuts.h"
+
+#define AMPPragmaDiagnosticPushWshadow \
+    _Pragma("clang diagnostic push"); \
+    _Pragma("clang diagnostic ignored \"-Wshadow\"");
+
+#define AMPPragmaDiagnosticPop \
+    _Pragma("clang diagnostic pop")
+
+#define AMPEmptyParameter
+
 #define AMPWeakify(object) \
     __weak typeof(object) weak_##object = object;
 
 #define AMPStrongify(object) \
-    __strong typeof(object) object = weak_##object; \
-    if (!object) { \
-        return; \
-    }
+    AMPPragmaPushWshadow \
+        __strong typeof(object) object = weak_##object; \
+    AMPPragmaPop
+
+#define AMPStrongifyAndReturnIfNil(object) \
+    AMPStrongifyAndReturnValueIfNil(object, AMPEmptyParameter)
+
+#define AMPStrongifyAndReturnNilIfNil(object) \
+    AMPStrongifyAndReturnValueIfNil(object, nil)
+
+#define AMPStrongifyAndReturnValueIfNil(object, value) \
+    AMPPragmaDiagnosticPushWshadow \
+        __strong typeof(object) object = weak_##object; \
+        if (!object) { \
+            return value; \
+        } \
+    AMPPragmaDiagnosticPop
 
 #endif /* AMPMarcos_h */
---
+++ AMPObjCUIStudy/Library/AMPRandom.m
@@ -0,0 +1,24 @@
+//
+//  AMPRandom.h
+//  AMPObjCStudy
+//
+//  Created by pilotns on 23.04.17.
+//  Copyright © 2017 pilotns. All rights reserved.
+//
+
+#import "AMPRandom.h"
+
+NSRange AMPMakeRange(NSUInteger firstNumber, NSUInteger lastNumber) {
+    NSUInteger minValue = MIN(firstNumber, lastNumber);
+    NSUInteger maxValue = MAX(firstNumber, lastNumber);
+    
+    return NSMakeRange(minValue, maxValue - minValue + 1);
+}
+
+NSUInteger AMPRandomValueUntilLocation(NSUInteger location) {
+    return arc4random_uniform((uint32_t)location);
+}
+
+NSUInteger AMPRandomValueWithRange(NSRange range) {
+    return arc4random_uniform((uint32_t)range.length) + range.location;
+}
