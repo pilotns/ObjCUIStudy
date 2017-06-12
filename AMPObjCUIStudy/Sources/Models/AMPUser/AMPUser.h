da07609216de93ba992c73319cf5ccb19e6caa59
pilotns <pilot.ns@icloud.com>
2017-06-12 12:42:54 +0300

    AMPUser

---
+++ AMPObjCUIStudy/Sources/Models/AMPUser/AMPUser.h
@@ -0,0 +1,14 @@
+//
+//  AMPUser.h
+//  AMPObjCUIStudy
+//
+//  Created by pilotns on 11.06.17.
+//  Copyright © 2017 pilotns. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface AMPUser : NSObject
+@property (nonatomic, readonly) NSString    *fullName;
+
+@end
---
+++ AMPObjCUIStudy/Sources/Models/AMPUser/AMPUser.m
@@ -0,0 +1,41 @@
+//
+//  AMPUser.m
+//  AMPObjCUIStudy
+//
+//  Created by pilotns on 11.06.17.
+//  Copyright © 2017 pilotns. All rights reserved.
+//
+
+#import "AMPUser.h"
+
+#import "NSString+AMPRandom.h"
+
+@interface AMPUser ()
+@property (nonatomic, strong)   NSString    *firstName;
+@property (nonatomic, strong)   NSString    *lastName;
+
+@end
+
+@implementation AMPUser
+
+@dynamic fullName;
+
+#pragma mark -
+#pragma mark Initializations and Deallocations
+
+- (instancetype)init {
+    self = [super init];
+    self.firstName = [NSString randomName];
+    self.lastName = [NSString randomName];
+    
+    return self;
+}
+
+#pragma mark -
+#pragma mark Accessors
+
+- (NSString *)fullName {
+    return [NSString stringWithFormat:@"%@ %@", self.firstName, self.lastName];
+}
+
+@end
