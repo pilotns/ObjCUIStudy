10b4e02e2bda242bc4ef2a9b2bdc55101c55d5a2
pilotns <pilot.ns@icloud.com>
2017-07-06 13:35:08 +0300

    NSFileManager+AMPExtensions

---
+++ AMPObjCUIStudy/Library/Categories/NSFileManager+AMPExtensions/NSFileManager+AMPExtensions.m
@@ -0,0 +1,53 @@
+//
+//  NSFileManager+AMPExtensions.m
+//  AMPObjCUIStudy
+//
+//  Created by pilotns on 05.07.17.
+//  Copyright © 2017 pilotns. All rights reserved.
+//
+
+#define AMPOnceURL(code) \
+    static NSURL *url = nil; \
+    static dispatch_once_t onceToken; \
+    dispatch_once(&onceToken, ^{ code }); \
+    \
+    return url;
+
+#define AMPURLForDirectoryWithSearchPathAndDomain(searchPath, domain) \
+    url = [self URLsForDirectory:searchPath inDomains:domain].firstObject;
+
+#define AMPURLForDirectoryInLibraryDirectoryCreateIfNeeded(directoryName) \
+    url = [[self URLForLibraryDirectory] URLByAppendingPathComponent:directoryName]; \
+    if (![[NSFileManager defaultManager] createDirectoryAtPath:url.path \
+                                   withIntermediateDirectories:YES \
+                                                    attributes:nil \
+                                                         error:nil]) { \
+        url = nil; \
+    }
+
+
+#import "NSFileManager+AMPExtensions.h"
+
+@implementation NSFileManager (AMPExtensions)
+
+- (NSURL *)URLForLibraryDirectory {
+    AMPOnceURL(AMPURLForDirectoryWithSearchPathAndDomain(NSLibraryDirectory, NSUserDomainMask));
+}
+
+- (NSURL *)URLForDocumentsDirectory {
+    AMPOnceURL(AMPURLForDirectoryWithSearchPathAndDomain(NSDocumentDirectory, NSUserDomainMask));
+}
+
+- (NSURL *)URLForDirectoryInLibraryDirectory:(NSString *)directoryName {
+    AMPOnceURL(AMPURLForDirectoryInLibraryDirectoryCreateIfNeeded(directoryName));
+}
+
+- (NSString *)fileNameWithURL:(NSURL *)url {
+    if (url.isFileURL) {
+        return url.lastPathComponent;
+    }
+    
+    return [url.path stringByReplacingOccurrencesOfString:@"/" withString:@"_"];
+}
+
+@end
